add6cb2fc312f4610e8875eade1a8f402b485f76
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-02 14:24:57 +0900

    新規作成したメモをファイルに保存

--- memo_app.rb
+++ memo_app.rb
@@ -2,8 +2,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-get '/' do
+def open_file
   File.open("memos_data.json") { |file| @memos = JSON.load(file) }
+end
+
+def create(params)
+  sample = []
+  @memos.keys.each { |memo| sample.push(memo.to_i) }
+  @memos[(sample.sort.last + 1).to_s] = { title: params["title"], description: params["description"] }
+  File.open("memos_data.json", "w") { |file| file.write(JSON.pretty_generate(@memos)) }
+end
+
+get '/' do
+  open_file
   erb :memos
 end
 
@@ -12,5 +23,7 @@ get '/memo/new' do
 end
 
 post '/memo/new' do
+  open_file
+  create(request.params)
   redirect '/'
 end
