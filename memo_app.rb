02cd4c757350458ffa0496cc86aae47b564d046f
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-02 22:09:53 +0900

    rubocopによる整形

--- memo_app.rb
+++ memo_app.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
 def open_file
-  File.open("memos_data.json") { |file| @memos = JSON.load(file) }
+  File.open('memos_data.json') { |file| @memos = JSON.parse(file.read) }
 end
 
 def write_file
-  File.open("memos_data.json", "w") { |file| file.write(JSON.pretty_generate(@memos)) }
+  File.open('memos_data.json', 'w') { |file| file.write(JSON.pretty_generate(@memos)) }
 end
 
 def create(title, description)
   sample = []
-  @memos.keys.each { |memo| sample.push(memo.to_i) }
-  @memos[(sample.sort.last + 1).to_s] = { title: title, description: description }
+  @memos.each_key { |memo| sample.push(memo.to_i) }
+  @memos[(sample.max + 1).to_s] = { title: title, description: description }
   write_file
 end
 
@@ -29,7 +31,7 @@ end
 
 helpers do
   def link_to(url, text)
-    %Q(<a href="#{url}">#{text}</a>)
+    %(<a href="#{url}">#{text}</a>)
   end
 end
 
@@ -44,34 +46,34 @@ end
 
 post '/memo' do
   open_file
-  create(escape_html(params["title"]), escape_html(params["description"]))
+  create(escape_html(params['title']), escape_html(params['description']))
   redirect '/'
 end
 
-get /\/memo\/([0-9]+)/ do
+get(%r{/memo/([0-9]+)}) do
   open_file
   @memo = [params['captures'].first, @memos[params['captures'].first]]
   erb :memo_show
 end
 
-get /\/memo\/([0-9]+)\/edit/ do
+get(%r{/memo/([0-9]+)/edit}) do
   open_file
   @memo = [params['captures'].first, @memos[params['captures'].first]]
   erb :memo_edit
 end
 
-patch /\/memo\/([0-9]+)/ do
+patch(%r{/memo/([0-9]+)}) do
   open_file
-  edit(escape_html(params["title"]), escape_html(params["description"]), params['captures'].first)
+  edit(escape_html(params['title']), escape_html(params['description']), params['captures'].first)
   redirect '/'
 end
 
-delete /\/memo\/([0-9]+)/ do
+delete(%r{/memo/([0-9]+)}) do
   open_file
   destroy(params['captures'].first)
   redirect '/'
 end
 
-get /\/.+/ do
+get(%r{/.+}) do
   erb :'404'
 end
