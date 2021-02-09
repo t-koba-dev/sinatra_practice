0e3e6d673e88f195b2b6a39a5fbb568e92594f19
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-09 14:45:46 +0900

    rubocopによる整形

--- memo_app.rb
+++ memo_app.rb
@@ -100,14 +100,14 @@ end
 get(%r{/memo/([0-9]+)}) do
   id = params['captures'].first
   memo = select_one_record(id)
-  @memo = { id: memo['id'],title: memo['title'], description: memo['description'] }
+  @memo = { id: memo['id'], title: memo['title'], description: memo['description'] }
   erb :memo_show
 end
 
 get(%r{/memo/([0-9]+)/edit}) do
   id = params['captures'].first
   memo = select_one_record(id)
-  @memo = { id: memo['id'],title: memo['title'], description: memo['description'] }
+  @memo = { id: memo['id'], title: memo['title'], description: memo['description'] }
   erb :memo_edit
 end
 
